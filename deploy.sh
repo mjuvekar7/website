9080f0bec58337646445f815016437e200ad3647
Mandar Juvekar <mjuvekar7@gmail.com>
2024-08-09 12:23:54 -0700

    Deploy script tweaks.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-deploy_dir="../www-compiled"
-site_dir="_site"
+deploy_dir="/Users/mandar/Devel/website/www-compiled"
+site_dir="/Users/mandar/Devel/website/www-jekyll/_site"
 
 echo "Wiping $deploy_dir."
 rm -rf $deploy_dir/*
